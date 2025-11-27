5265810a18b498ea899e4cd81f7a9f34fc4e3dbf
venith-creator <patrickaghidi222005@gmail.com>
2025-11-27 19:21:36 +0100

    contact

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -23,7 +23,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ContactView.vue'),
+      component: () => import('../views/Contactview.vue'),
     },
   ],
 })
